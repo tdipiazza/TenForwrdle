97f6106011fc1ebc28acd630bac0db9a940c6257
Tricia <tricia@tenforwardconsulting.com>
2022-02-08 08:02:50 -0600

    Initial Commit

---
+++ Shared/TenForwrdleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenForwrdleApp.swift
+//  Shared
+//
+//  Created by Tricia DiPiazza on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TenForwrdleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
